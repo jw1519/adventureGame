a7d3ff2ff71182e8a09262a61453fe799445c18b
Jess Woodward <jw1519@student.aru.ac.uk>
2023-12-05 13:53:58 +0000

    update

--- adventureGame/Program.cs
+++ adventureGame/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace adventureGame
 {
@@ -10,7 +11,10 @@ namespace adventureGame
         static void Main(string[] args)
         {
             bool isRunning = true;
-            
+            bool hasStaff = false;
+            bool goShop = false;
+
+            bool EndGame = false;
             
             
             string playerChoice = "";
@@ -47,71 +51,91 @@ namespace adventureGame
             Console.WriteLine($"Charisma: {charisma}");
             Console.WriteLine($"Strength: {strength}");
 
+            Restart();
+         
+           
 
-            
-            while (isRunning == true)
+            //Console.WriteLine("Do you want to quit or restart (quit/restart)");
+            //playerChoice = Console.ReadLine().ToLower();
+
+
+            //if (playerChoice == "restart")
+            //{
+            //    Restart();
+            //}
+            //else
+            //{
+            //    EndGame = true;
+            //}
+             void Restart()
             {
-                //Story and choices
-                Console.WriteLine("you begin your adventure"); //maybe use keys
-                Console.WriteLine("you find yourself at a fork in the road. Do you go left or right?");
-                playerChoice = Console.ReadLine().ToLower();
-
-                if (playerChoice == "left")
+                while (isRunning == true)
                 {
-                    Console.WriteLine("You see a man infront of you. Are you worthy for this Staff?");
-                    Console.WriteLine("do you take the staff? (take/leave)");
-
+                    //Story and choices
+                    Console.WriteLine("you begin your adventure"); //maybe use keys
+                    Console.WriteLine("you find yourself at a fork in the road. Do you go left or right?");
                     playerChoice = Console.ReadLine().ToLower();
 
-                    if (playerChoice == "take")
+                    if (playerChoice == "left")
                     {
-                        //Console.WriteLine("you need to fight me for it");
+                        Console.WriteLine("You see a man infront of you. Are you worthy for this Staff?");
+                        Console.WriteLine("do you take the staff? (take/leave)");
 
+                        playerChoice = Console.ReadLine().ToLower();
 
-                        if (isMagicUser == true)
+                        if (playerChoice == "take")
                         {
-                            Console.WriteLine("you grab the staff and shoot a bolt at the tree. The wizard congratulates you!");
-                            isRunning = false;
+                            //Console.WriteLine("you need to fight me for it");
+
+
+                            if (isMagicUser == true)
+                            {
+                                Console.WriteLine("you grab the staff and shoot a bolt at the tree. The wizard congratulates you!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("you grab the staff but nothing happens. The wizard takes it back as you are not worthy");
+                                hasStaff = true;
+                            }
+                        }
+                        else if (playerChoice == "leave")
+                        {
+                            Console.WriteLine("the wizard sighs as you walk away");
                         }
                         else
                         {
-                            Console.WriteLine("you grab the staff but nothing happens. The wizard takes it back as you are not worthy");
-                            isRunning = false;
+                            Console.WriteLine("the wizard stares at you and walks away");
                         }
-
                     }
-                    else if (playerChoice == "leave")
+                    else if (playerChoice == "right")
                     {
-                        Console.WriteLine("the wizard sighs as you walk away");
-                        isRunning = false;
+                        Console.WriteLine("You find a treasure chest at the side of the road");
                     }
                     else
                     {
-                        Console.WriteLine("the wizard stares at you and walks away");
+                        Console.WriteLine("you wander aimlessly and get lost. Game over!");
+                    }
+
+                    if (goShop == true)
+                    {
+                        Console.WriteLine("Welcome to the shop what can I get for you?");
+                        Console.WriteLine("potions, weapons, shields");
+                    }
+
+                    Console.WriteLine("Do you want to quit or restart (quit/restart)");
+                    playerChoice = Console.ReadLine().ToLower();
+
+
+                    if (playerChoice == "restart")
+                    {
+                        Restart();
+                    }
+                    else
+                    {
                         isRunning = false;
                     }
                 }
-                else if (playerChoice == "right")
-                {
-                    Console.WriteLine("You find a treasure chest at the side of the road");
-                    isRunning = false;
-                }
-                else
-                {
-                    Console.WriteLine("you wander aimlessly and get lost. Game over!");
-                    isRunning = false;
-                }
             }
-            Console.WriteLine("Do you want to quit or restart (quit/restart)");
-
-            if (playerChoice == "restart")
-            {
-                isRunning = true;
-            }
-            else
-            {
-                isRunning = false;
-            }
-        }
+        } 
     }
 }
